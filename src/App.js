42788b468698c250b49abc6b73a6b8c97f2e2f8f
rohit1101 <srohi1101@gmail.com>
2020-07-21 22:48:28 +0530

    M0 commit
    prettierc setup
    
    prettierc setup
    
    checking prehook
    
    installed husky
    
    checking prehook
    
    checking prehook
    
    initial commit
    
    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>GoRetro</h1>
+      <h2>Get Started</h2>
+    </div>
+  );
+}
